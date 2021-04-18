d0600380bb970a73c7ef06f56b5ca89da6a7c33d
khalld <37438774+khalld@users.noreply.github.com>
2021-04-18 22:05:36 +0200

    update test

--- project/test.py
+++ project/test.py
@@ -1,5 +1,6 @@
 ## test per file manager
-
+from os import path
+import numpy as np
 
 # classes rispettando l'ordine rispettivamente
 # coast
@@ -15,4 +16,43 @@
 # insidecity_art165.jpg, 3
 # street_urb382.jpg, 6
 # opencountry_land666.jpg, 5
+
+def main():
+
+    class_dict = {
+        "empty": -1,
+        "half": 0,
+        "full": 1
+    }
+
+    for key in class_dict:
+        print(key, '-->', class_dict[key])
+
+    path = "static/datasets/"
+
+    # read 
+    reader = np.loadtxt(path + 'test.txt', dtype=str) # , delimiter=','
+    print(reader)
+
+    # write
+    test_dict = {
+        "test_file1": 0,
+        "test_file2": -1,
+        "test_file4": 1,
+        "test_fil9": 0
+    }
+
+    writer = open(path + 'train.txt', 'w')
+
+    for key in test_dict:
+        writer.write(key + ", " + str(test_dict[key]))
+        writer.write("\n")
+
+    writer.close()
+
+    reader2 = np.loadtxt(path + 'train.txt', dtype=str)
+    print(reader2)
+
+
+main()
 
