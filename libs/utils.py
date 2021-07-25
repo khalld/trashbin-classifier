186243ce93cdec42dab9191b8dffd0fdd4105d01
khalld <37438774+khalld@users.noreply.github.com>
2021-07-25 17:19:11 +0200

    update

--- libs/utils.py
+++ libs/utils.py
@@ -1,6 +1,6 @@
-from torchvision import transforms
 from os.path import join
 from TDContainer import TDContainer
+from sklearn.model_selection import train_test_split
 
 def get_model_name(model_name, version):
     return "%s_%s" % (model_name, version)
@@ -22,3 +22,8 @@ def import_dataset(path_dst: str, train_transform, test_transform):
         }
 
     return TDContainer(training=dst_train, validation=dst_validation, test=dst_test)
+
+def split_train_val_test(dataset, perc=[0.6, 0.1, 0.3]):
+    train, testval = train_test_split(dataset, test_size = perc[1]+perc[2])
+    val, test = train_test_split(testval, test_size = perc[2]/(perc[1]+perc[2]))
+    return train, val, test
