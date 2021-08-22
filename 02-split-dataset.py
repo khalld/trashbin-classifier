182fc238f90b792b17032994664bc8583715c86a
khalld <37438774+khalld@users.noreply.github.com>
2021-08-22 18:08:37 +0200

    revisione codice 02

--- 02-split-dataset.py
+++ 02-split-dataset.py
@@ -3,51 +3,37 @@ import random
 import numpy as np
 import pandas as pd
 
-from PIL import Image
 from libs.utils import split_train_val_test
 
-def main():
+if __name__ == "__main__":   
     random.seed(1996)
     np.random.seed(1996)
 
-    print("\n\n")
-
+    # load all_labels.txt
     all_labels_txt = np.loadtxt('dataset/all_labels.txt', dtype=str, delimiter=',')
 
-    print('length of dst: %d, shape %s,\nex coppia: filename: %s, class: %s' %( len(all_labels_txt), all_labels_txt.shape, all_labels_txt[0][0], all_labels_txt[0][1] ) )
+    print('Length of dataset:\n %d, shape %s,\nexample of couple: (filename: %s, class: %s)' %( len(all_labels_txt), all_labels_txt.shape, all_labels_txt[0][0], all_labels_txt[0][1] ) )
 
     images = []
     labels = []
 
     for i in range(len(all_labels_txt)):
-        images.append('dataset/images/' + all_labels_txt[i][0]) # creo un array di label
-        labels.append(all_labels_txt[i][1]) # array di classe di appartenenza
+        # create a str array that contains frames 
+        images.append('dataset/images/' + all_labels_txt[i][0])
+        # create a int array that contains frames's class
+        labels.append(all_labels_txt[i][1])
 
-    # print(len(images), len(labels))
+    print('Tot images %d, tot labels %d' %(len(images), len(labels)))
 
+    # instantiate dataframe
     dataset_df = pd.DataFrame({
         'image': images,
         'label': labels
     })
 
-    # test corretto caricamento
-    # print(dataset_df['label'][0])
-    # Image.open(dataset_df['image'][0]).show()
-
-    # stampo i primi 5 elementi del dataFrame
-    # print(dataset_df.head())
-
     training_df, validation_df, test_df = split_train_val_test(dataset=dataset_df)
 
-    # check distribuzione di classi
-    print("training", training_df['label'].value_counts() ) 
-    print("\n")
-    print("validation", validation_df['label'].value_counts() ) 
-    print("\n")
-    print("test", test_df['label'].value_counts() )
-    print("\n")
-
-
+    # save to csv
     training_df.to_csv('dataset/training.csv', index=None)
     validation_df.to_csv('dataset/validation.csv', index=None)
     test_df.to_csv('dataset/test.csv', index=None)
@@ -60,5 +46,3 @@ def main():
     }.items())
     ids = pd.DataFrame({'id':ids, 'class':classes}).set_index('id')
     ids.to_csv('dataset/classes.csv')
-
-main()
