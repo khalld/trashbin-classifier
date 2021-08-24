482fe6791487d0b80ce37d63b6c900cf3abde377
khalld <37438774+khalld@users.noreply.github.com>
2021-08-25 01:23:10 +0200

    general update

--- libs/utils.py
+++ libs/utils.py
@@ -3,6 +3,7 @@ from os.path import join
 from torchvision import transforms
 from libs.TDContainer import TDContainer
 from sklearn.model_selection import train_test_split
+from libs.PretrainedModels import PretrainedModelsCreator
 
 def get_model_name(model_name: str, lr: str):
     """Return a string that contain a model name and larning ratee"""
@@ -30,3 +31,16 @@ def split_train_val_test(dataset, perc=[0.6, 0.1, 0.3]):
     train, testval = train_test_split(dataset, test_size = perc[1]+perc[2])
     val, test = train_test_split(testval, test_size = perc[2]/(perc[1]+perc[2]))
     return train, val, test
+
+
+def reverse_norm(im):
+    """Allow to show a normalized image"""
+    
+    im = im-im.min()
+    return im/im.max()
+
+def init_model(creator: PretrainedModelsCreator, model_name: str, num_classes: int = 3, feature_extract: bool=True, use_pretrained: bool = True):
+    print('Initializing: %s' % (model_name))
+    creator.init_model(num_classes=num_classes, model_name=model_name, feature_extract=feature_extract, use_pretrained=use_pretrained)
+    # creator.get_info()
+    return creator
