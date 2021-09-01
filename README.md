80ede02828a824825b0878f7a47d5835a1663f9e
khalld <37438774+khalld@users.noreply.github.com>
2021-09-01 20:13:58 +0200

    update

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# machine-learning-unict-20202021
+# Trash bin classifier
 
-## Create virtual environment
+Project c.....
+
+Dataset is available [here](https://drive.google.com/drive/folders/1LmN-fXWZ8UpRkLeMjbootN46V9AHaE4x?usp=sharing)
+
+## Setup
+
+Create virtual environment
 
 `python3 -m venv venv`
 
@@ -11,6 +17,12 @@ Activate venv
 Install requirements libs
 
 `pip install -r requirements.txt`
+
+Run app
+
+`python3 app.py`
+
+## Utils
 
 Exit from venv
 
--- app.py
+++ app.py
@@ -3,32 +3,38 @@ import torch
 import torchvision.models as models
 from torchvision import transforms
 from PIL import Image
-from libs.PretrainedModels import PretrainedModelsCreator, SqueezeNet1_cc
+from libs.PretrainedModels import PretrainedModelsCreator, SqueezeNet_cc
+from libs.utils import load_model
 
-                                                        ## di default carico un modello da mettere nella repo per test antonio
-def init(creator: PretrainedModelsCreator, path_mdl: str='SqueezeNet1_1__lr=0.00282-50.pth'):
+def init(creator: PretrainedModelsCreator, path_mdl: str='model_trained.pth'):
     #print('Instantiating ' )
     #print(type(creator))
-    creator.init_model(num_classes=3, model_name='SqueezeNet_v1_1', feature_extract=True, use_pretrained=True)
+    creator.init_model(num_classes=3, model_name='SqueezeNet', feature_extract=True, use_pretrained=True)
     #print parameters to be sure that are different model
     #creator.get_info()
     #creator.get_parameters()
     #print("\n\n\n\n")
-    creator.load_model(path=path_mdl)
+    creator.load_from_file(path=path_mdl)
     #creator.get_parameters()
 
     return creator.ret_model()
 
-my_model = init(creator=SqueezeNet1_cc())
+my_model = init(creator=SqueezeNet_cc())
 # print(my_model, type(my_model))
 my_model.eval()
-# alexnet = models.alexnet(pretrained=True).eval()
 
 raw_labels = str({0: 'empty',1: 'half',2: 'full'})
 labels = [i.split(',')[0] for i in list( eval(raw_labels).values() )]
 
 def inference(data):
-    image = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()])(Image.fromarray(data.astype('uint8'), 'RGB')).unsqueeze(0)
+    image = transforms.Compose([
+        transforms.Resize(320),
+        # wastebin will be centered in the photo (photo from smartphone has usually height > width)
+        # use centerCrop to exclude other parts of images not necessary, like floor
+        # tried also witouth centerCrop, using centerCrop gives better result
+        transforms.CenterCrop(224),
+        transforms.ToTensor()
+    ])(Image.fromarray(data.astype('uint8'), 'RGB')).unsqueeze(0)
     prediction = torch.nn.functional.softmax(my_model(image)[0], dim=0)
 
     return dict(zip(labels, map(float, prediction)))
